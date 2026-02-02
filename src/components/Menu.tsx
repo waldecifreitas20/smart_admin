7e49cee891c105c850cebda27c21f9bdbcc95595
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-02 08:03:13 +0000

    feat: add lucide icons

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,6 @@
 export function Menu() {
     return (
-        <aside className="bg-white border-t border-purple-500 px-4 fixed bottom-0 w-full">
+        <aside className="bg-white border-t border-purple-500 px-4 fixed bottom-0 w-full shadow-top shadow-2xl">
             <nav>
                 <ul className="flex justify-between leading-16">
                     <li className="hover:text-purple-500">Dashboard</li>

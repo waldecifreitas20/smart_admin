352f6dc1e701a742882cd1ef946a855311a84bc4
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-02 08:52:55 +0000

    feat: dashboard cards

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,10 +1,35 @@
+import { Activity, ShieldCheck, UserRoundX } from "lucide-react";
 import { SearchBar } from "../components/SearchBar";
 
 export function Dashboard() {
+    const dashboardStats = [
+        {icon:<Activity size={16} />, label: "Cobertura", value: "100%" },
+        {icon: <UserRoundX size={16} />, label: "Indiponíveis", value: "100%" },
+        {icon: <ShieldCheck size={16} />, label: "Ativos", value: "100%" },
+    ];
+
     return (
-        <div>
-            <SearchBar />
-            <h1>Escala Semanal</h1>
+        <>
+        <SearchBar />
+        <div className="mt-12">
+            <h1 className="text-4xl text-slate-950 font-bold">Painel de Escalas</h1>
+            <p>Visualize e gerencie as escalas dos colaboradores ativos</p>
         </div>
+
+        <div className="grid grid-cols-3 gap-2 mt-8">
+            {dashboardStats.map((stat) => (
+                <div key={stat.label} 
+                    className="
+                    bg-white p-2 rounded-lg 
+                    shadow-sm border border-slate-100
+                    flex flex-col items-start justify-bewteen h-full
+                    ">
+                    <h2 className="text-sm text-slate-600 block h-full flex items-center gap-2">{stat.label} {stat.icon}</h2>
+                    <p className="text-2xl text-purple-500 font-bold mt-2">{stat.value}</p>
+                </div>
+            ))}
+            
+        </div>
+        </>
     );
 }

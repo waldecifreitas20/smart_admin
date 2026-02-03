3af4b25a076759ce05af9a4df9aea072d07e34da
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-03 06:25:24 +0000

    feat: add outlined button

---
+++ src/components/OutlinedButton.tsx
@@ -0,0 +1,22 @@
+import type { PropsWithChildren } from "react";
+
+interface OutlinedButtonProps extends PropsWithChildren { }
+
+export function OutlinedButton(props: OutlinedButtonProps) {
+  return (
+    <button
+      className="
+      border border-purple-600
+      text-purple-600 
+      flex items-center justify-center 
+      gap-2 text-sm
+       
+      px-4 py-3 w-full
+      rounded-md 
+      cursor-pointer
+      hover:bg-purple-200 transition-all
+    ">
+      {props.children}
+    </button>
+  );
+}

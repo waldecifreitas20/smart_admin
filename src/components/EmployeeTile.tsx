cb1b5e833c0f032ca1f028f9931c3922b12a9fa5
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-04 03:34:37 +0000

    feat: add dsr style

--- src/components/EmployeeTile.tsx
+++ src/components/EmployeeTile.tsx
@@ -2,14 +2,16 @@ import type { Employee } from "../types/Employee";
 
 interface EmployeeTileProps {
   employee: Employee;
+  isDayoff?: boolean;
 }
 
-const bgColors = ["bg-blue-100", "bg-green-100", "bg-yellow-100", "bg-red-100", "bg-purple-100", "bg-pink-100"];
-const textColors = ["text-blue-700", "text-green-700", "text-yellow-700", "text-red-700", "text-purple-700", "text-pink-700"];
+const bgColors = ["bg-blue-100", "bg-emerald-100", "bg-green-100", "bg-red-100", "bg-purple-100", "bg-pink-100", "bg-yellow-100", "bg-teal-100", "bg-indigo-100"];
+const textColors = ["text-blue-700", "text-emerald-700", "text-green-700", "text-red-700", "text-purple-700", "text-pink-700", "text-yellow-700", "text-teal-700", "text-indigo-700"];
 
 export function EmployeeTile(props: EmployeeTileProps) {
-  const selectedBackground = bgColors[Math.floor(Math.random() * bgColors.length)];
-  const selectedTextColor = textColors[Math.floor(Math.random() * textColors.length)];
+  const colorIndex = Math.floor(Math.random() * bgColors.length);
+  const selectedBackground = bgColors[colorIndex];
+  const selectedTextColor = textColors[colorIndex];
 
   return (
     <div className="
@@ -33,10 +35,14 @@ export function EmployeeTile(props: EmployeeTileProps) {
           <p className="text-xs text-slate-500">edv: {props.employee.edv}</p>
         </div>
 
+
         <p className={`
-           px-3 py-1 rounded-sm text-xs ${selectedTextColor}
-           ${selectedBackground}
-          `}>{props.employee.shiftIn} - {props.employee.shiftOut}</p>
+           px-3 py-1 rounded-sm text-xs ${props.isDayoff ? "text-slate-700" : selectedTextColor}
+           ${props.isDayoff ? "bg-slate-100" : selectedBackground}
+          `}>{props.isDayoff ?
+            "Folga" :
+            `${props.employee.shiftIn} - ${props.employee.shiftOut}`}</p>
+
 
       </span>
     </div>

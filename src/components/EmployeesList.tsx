aa2180b15bcf67f2b40f7e862f7df0f3723f7ac7
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2026-02-04 08:30:32 +0000

    feat: create add employee manually btn

--- src/components/EmployeesList.tsx
+++ src/components/EmployeesList.tsx
@@ -7,6 +7,18 @@ interface EmployeesListProps {
 
 export function EmployeesList(props: EmployeesListProps) {
 
+  if (props.employees.length === 0) {
+    return (
+      <>
+      <p className=" w-full text-center text-slate-300 mt-4">
+        Nenhum colaborador escalado para esta data.
+      </p>
+
+      <button className="mt-4 w-full text-sm text-purple-500 hover:text-purple-700">Adicionar Colaborador</button>
+      </>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4 mt-2">
       {props.employees.map((employee, index) =>  (
